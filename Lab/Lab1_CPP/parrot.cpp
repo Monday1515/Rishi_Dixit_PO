c299aae1dba989d16897fde42a4186750bb5ef45
r1sh11 <ryshi.dixit@gmail.com>
2024-10-20 18:57:41 +0200

    lab1 task 4.5

--- Lab/Lab1_CPP/parrot.cpp
+++ Lab/Lab1_CPP/parrot.cpp
@@ -1,33 +1,49 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 class Parrot {
 private:
-    string phrase;
+    vector<string> phrase;
 public:
     Parrot(string Phrase) {
-        phrase = Phrase;
+        phrase.push_back(Phrase);
     }
 
-    void say(int repetition){
-        for (int i=0; i< repetition; i++) {
-            cout << phrase << endl;
+    void say(int repetition = 1) {
+        if (!phrase.empty()) {
+            for (int i = 0; i < repetition; i++) {
+                int random = rand() % phrase.size();
+                cout << phrase[random] << endl;
+            }
         }
     }
 
-    void change(string newPhrase) {
-        phrase = newPhrase;
+    void addPhrase(string newPhrase) {
+
+        phrase.push_back(newPhrase);
+
     }
+
+
 };
+
 
 int main() {
 
+    srand(time(0));
     Parrot Parrot("Hello World"); 
     Parrot.say(1);
 
-    Parrot.change("Hello");
-    Parrot.say(3);
+
+    Parrot.addPhrase("Hello World");
+    Parrot.addPhrase("Hello");
     
+    Parrot.say(1);
+    Parrot.say(1);
     return 0;
 }
