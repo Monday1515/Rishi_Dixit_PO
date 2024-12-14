c09c0c241ce30fc14dcecb0bd3ff2bb7049d316c
r1sh11 <ryshi.dixit@gmail.com>
2024-12-14 19:49:22 +0100

    Lab 7 CPP task 1-2

---
+++ Lab/Lab7_CPP/OCP.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+// Interfejs rabatu
+class Discount {
+public:
+    virtual double discount(double price) const = 0;
+    virtual ~Discount() = default;
+};
+
+// Regularny rabat
+class RegularDiscount : public Discount {
+public:
+    double discount(double price) const override {
+        return price = price * 0.9;
+    }
+};
+
+// Sezonowy rabat
+class SeasonalDiscount : public Discount {
+public:
+    double discount(double price) const override {
+        return price = price * 0.8;
+    }
+};
+
+void putDiscount(const Discount& discount, double& price) {
+    cout << discount.discount(price) << endl;
+}
+
+int main() {
+    double price = 50.0;
+
+    RegularDiscount regularDiscount;
+    SeasonalDiscount seasonalDiscount;
+
+    putDiscount(regularDiscount, price);
+
+    putDiscount(seasonalDiscount, price);
+    
+    return 0;
+}
---
+++ Lab/Lab7_CPP/SRP.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+// Klasa użytkownika
+class User {
+private:
+    string name;
+    string email;
+    string phoneNumber;
+
+public:
+    User(const string& name, const string& email, const string& phoneNumber) : name(name), email(email), phoneNumber(phoneNumber) {}
+
+    string getName() const {
+        return name;
+    }
+
+    string getEmail() const {
+        return email;
+    }
+
+    string getPhoneNumber() const {
+        return phoneNumber;
+    }
+
+    void setName(const string& newName) {
+        name = newName;
+    }
+
+    void setEmail(const string& newEmail) {
+        email = newEmail;
+    }
+
+    void setPhoneNumber(const string& newPhoneNumber) {
+        phoneNumber = newPhoneNumber;
+    }
+
+};
+
+
+// Klasa odpowiedzialna za zapis użytkownika
+class UserSaver {
+private:
+    string File;
+
+public:
+
+    UserSaver(const string& File) : File(File) {}
+
+    void saveUser(const User& user) {
+        fstream file(File, ios::app);
+        
+        if (file.is_open()) {
+            file << "Name: " << user.getName() << endl << "Email: " << user.getEmail() << endl << "Phone Number: " << user.getPhoneNumber() << "\n" << endl;
+            file.close();
+        } 
+    }
+};
+
+int main() {
+    
+    User user("Jan Nowak", "jan.nowak@abc.pl", "123456789");
+
+    User user2("Pawel Kowalski", "pawel.kowalski@abc.pl", "987654321");
+
+    UserSaver saver("file1.txt");
+
+    saver.saveUser(user);
+    saver.saveUser(user2);
+    return 0;
+}
